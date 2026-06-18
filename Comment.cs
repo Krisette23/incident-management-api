a1b6195222dec8bab394b273b0caabd791aa8d9e
Cristina Sanchez Lopez <s192887@student.hb.se>
2026-06-18 16:55:35 +0200

    Add project files.

---
+++ Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace incidentmanagement
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int IncidentId { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Incident.cs
@@ -0,0 +1,14 @@
+﻿namespace incidentmanagement
+{
+    public class Incident
+    {   public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int CreatedByUserId { get; set; }
+        public int? AssignedToUserId { get; set; }
+    }
+    
+}
