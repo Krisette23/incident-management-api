b7a32d28b4e428cf98d8eb511bfa58afe39f263f
Cristina Sanchez Lopez <s192887@student.hb.se>
2026-06-22 16:37:34 +0200

    API config

---
+++ Controllers/IncidentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using incidentmanagement.Models;
+using incidentmanagement.Database;
+
+namespace incidentmanagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncidentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public IncidentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _context.Incidents.ToListAsync());
+        }
+    }
+}
