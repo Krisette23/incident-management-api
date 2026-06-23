7c557eece17b3b755ba4a8103f26d5b9ff6e5953
Cristina Sanchez Lopez <s192887@student.hb.se>
2026-06-23 14:17:24 +0200

    get by id and Post Create

--- Controllers/IncidentController.cs
+++ Controllers/IncidentController.cs
@@ -20,5 +20,24 @@ namespace incidentmanagement.Controllers
         {
             return Ok(await _context.Incidents.ToListAsync());
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var incident = await _context.Incidents.FindAsync(id);
+            if (incident == null)
+            {
+                return NotFound();
+            }
+            return Ok(incident);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Incident incident)
+        {
+            _context.Incidents.Add(incident);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = incident.Id }, incident);
+        }
     }
 }
