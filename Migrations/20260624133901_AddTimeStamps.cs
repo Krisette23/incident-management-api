5c134b229e7f89964a4d1ce2a2527aebe9b39a3f
Cristina Sanchez Lopez <s192887@student.hb.se>
2026-06-24 15:39:29 +0200

    Added Timestamps to the database

---
+++ Migrations/20260624133901_AddTimeStamps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace incidentmanagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTimeStamps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Incidents",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Incidents");
+        }
+    }
+}
