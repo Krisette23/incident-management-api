ff0364412c5954fed8e8fcda7adf23457bd1ef40
Cristina Sanchez Lopez <s192887@student.hb.se>
2026-06-24 15:52:54 +0200

    created auth files

---
+++ Auth/JwSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace incidentmanagement.Auth
+{
+    public class JwSettings
+    {
+        public string SecretKey { get; set; }
+        public string Issuer { get; set; } 
+        public string Audience { get; set; }
+    }
+}
