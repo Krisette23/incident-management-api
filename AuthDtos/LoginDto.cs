d5ebb19c18189519dde7488dc174cb526a35b12b
Cristina Sanchez Lopez <s192887@student.hb.se>
2026-06-24 16:26:08 +0200

    Auth users and database

---
+++ AuthDtos/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace incidentmanagement.AuthDtos
+{
+    public class LoginDto
+    {
+          public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Migrations/20260624142341_AdduserAuth.Designer.cs
+++ Migrations/20260624142341_AdduserAuth.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using incidentmanagement.Database;
 
@@ -11,9 +12,11 @@ using incidentmanagement.Database;
 namespace incidentmanagement.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260624142341_AdduserAuth")]
+    partial class AdduserAuth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
